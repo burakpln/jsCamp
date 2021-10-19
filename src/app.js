debdbee69c61a96a43831ec7a4ca4799fdafb87c
burakpln <burak.pln@hotmail.com>
2021-10-20 00:03:13 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+console.log("Hello World")
+
+let dolarDun = 9.20;
+let dolarBugun = 9.30;
+
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi"]
+console.log("<ul>")
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>", konutKredileri[i], "</li>")
+}
+console.log("</ul>")
+
+console.log(dolarDun)
+
